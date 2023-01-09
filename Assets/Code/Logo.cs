7ca17635150427f09e50751fe9141ed6e65ae4df
Ruslan Kibets <ruslan.kibets@gmail.com>
2023-01-09 11:26:09 -0800

    Logo

---
+++ Assets/Code/Logo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Logo : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(LogoRoutine());
+    }
+
+    private IEnumerator LogoRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+        
+        transform.DOLocalMoveY(20f, 0.79f).SetEase(Ease.InBack);
+        Sounds.Instance.PlayExact("ui_swing_1");
+
+    }
+}
